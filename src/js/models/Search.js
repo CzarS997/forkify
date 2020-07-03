ccd8a79c8f65a037a9436be878c1cdd48dd53fbe
MFella <51965993+MFella@users.noreply.github.com>
2020-07-03 22:36:55 +0200

    little changes

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,7 +10,8 @@ export default class Search{
         try{
        const key = `1750ed86acb189eed267f2ff3e6de5fd`;
        const proxy = 'https://cors-anywhere.herokuapp.com/';
-       const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+       //const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+       const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
        //console.log(res);
        this.result = res.data.recipes;
        //console.log(this.result);
